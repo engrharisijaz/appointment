c5276813e20aa4ecd74d6e52f7598cd9c2b2d6c5
engrharisijaz <92294452+engrharisijaz@users.noreply.github.com>
2024-05-21 18:22:38 +0500

    Create generate_token.py

---
+++ generate_token.py
@@ -0,0 +1,26 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+import os
+
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+def main():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+if __name__ == "__main__":
+    main()
